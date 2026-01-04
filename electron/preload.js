ddfb16cf1e1064bb3afd40a61c2ace790180ea1f
alzaabi555 <157557210+alzaabi555@users.noreply.github.com>
2026-01-04 23:56:30 +0400

    Add files via upload

--- electron/preload.js
+++ electron/preload.js
@@ -1,6 +1,8 @@
 
 const { contextBridge, shell } = require('electron');
 
+// نحن نكشف فقط وظيفة 'openExternal' للواجهة الأمامية
+// هذا يحمي التطبيق من الوصول الكامل للنظام مع السماح بفتح الروابط
 contextBridge.exposeInMainWorld('electron', {
   openExternal: (url) => shell.openExternal(url)
 });
